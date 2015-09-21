06aee2a18fc3610617363ff3e5b7ee1be7008a32
Carl Sverre <accounts@carlsverre.com>
2015-09-21 12:28:03 -0700

    Migrating examples to the streamliner-examples repo

--- Transformers.scala
+++ Transformers.scala
@@ -1,50 +1,23 @@
 package com.memsql.streamliner.starter
 
-import org.apache.spark._
-import org.apache.spark.rdd._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.dstream._
-import org.apache.spark.sql._
-import org.apache.spark.sql.types._
-import com.memsql.spark.context._
-import com.memsql.spark.connector._
-import com.memsql.spark.etl.api._
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.{Row, DataFrame, SQLContext}
+import org.apache.spark.rdd.RDD
+import com.memsql.spark.etl.api.{ UserTransformConfig, SimpleByteArrayTransformer }
 import com.memsql.spark.etl.utils.PhaseLogger
 
-// A Transformer implements the transform method which turns an RDD into a DataFrame.
-class EvenNumbersOnlyTransformer extends SimpleByteArrayTransformer {
+// This transformer expects an input RDD containing ints, and returns a
+// dataframe with one row per int
+class BasicTransformer extends SimpleByteArrayTransformer {
   override def transform(sqlContext: SQLContext, rdd: RDD[Array[Byte]], config: UserTransformConfig, logger: PhaseLogger): DataFrame = {
     logger.info("transforming the RDD")
 
     // transform the RDD into RDD[Row]
-    val integerRDD = rdd.map(byteUtils.bytesToInt)
-    val filteredRDD = integerRDD.filter(x => x % 2 == 0)
-    val transformedRDD = filteredRDD.map(x => Row(x))
+    val integerRDD = rdd.map(byteUtils.bytesToInt).map(Row(_))
 
     // create a schema with a single non-nullable integer column named number
     val schema = StructType(Array(StructField("number", IntegerType, true)))
 
-    sqlContext.createDataFrame(transformedRDD, schema)
-  }
-}
-
-// A Transformer can also be configured with the config blob that is provided in MemSQL Ops.
-class ConfigurableNumberParityTransformer extends SimpleByteArrayTransformer {
-  override def transform(sqlContext: SQLContext, rdd: RDD[Array[Byte]], config: UserTransformConfig, logger: PhaseLogger): DataFrame = {
-    var keepEvenNumbers = config.getConfigBoolean("filter", "even").getOrElse(true)
-    var keepOddNumbers = config.getConfigBoolean("filter", "odd").getOrElse(true)
-    var columnName = config.getConfigString("table", "column_name").getOrElse("number")
-
-    logger.info("transforming the RDD")
-
-    // transform the RDD into RDD[Row] using the configured filter
-    val integerRDD = rdd.map(byteUtils.bytesToInt)
-    val filteredRDD = integerRDD.filter(x => (x % 2 == 0 && keepEvenNumbers) || (x % 2 == 1 && keepOddNumbers))
-    val transformedRDD = filteredRDD.map(x => Row(x))
-
-    // create a schema with a single non-nullable integer column using the configured column name
-    val schema = StructType(Array(StructField(columnName, IntegerType, true)))
-
-    sqlContext.createDataFrame(transformedRDD, schema)
+    sqlContext.createDataFrame(integerRDD, schema)
   }
 }
